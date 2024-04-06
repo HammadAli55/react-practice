d926913518daadfd0aabb1f8d95243dc238e33ff
HammadAli55 <hammadalii@outlook.com>
2024-04-07 01:00:10 +0500

    implement routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import Todo from './components/Todo';
+import { Route, Routes } from "react-router-dom";
+import Todo from "./components/Todo";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetups from "./pages/NewMeetups";
+import Favorites from "./pages/Favorites";
 
 function App() {
   return (
-   <div>
-    <h1>
-      My Todos
-    </h1>
-      <Todo text="one"/>
-      <Todo text="two"/>
-   </div>
+    <div>
+      <Routes>
+        <Route path="" element={<AllMeetupsPage />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/new" element={<NewMeetups />} />
+      </Routes>
+    </div>
+
+    //  <div>
+    //   <h1>
+    //     My Todos
+    //   </h1>
+    //     <Todo text="one"/>
+    //     <Todo text="two"/>
+    //  </div>
   );
 }
 
